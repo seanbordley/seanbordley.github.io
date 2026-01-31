61034ed7ee084de0d0a0e363bf03338d82c6b8b6
Sean Bordley <seanbordley@gmail.com>
2026-01-31 00:21:33 -0800

    Update script.js

--- site_files/school/script.js
+++ site_files/school/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $("#start-animation").click(function() {
-        
         const heading = $("#main-heading");
         const image = $("#profile-img");
 
-        heading.animate({ fontSize: "2.5em" }, 1000)
+        heading.animate({ fontSize: "2.4em" }, 1000)
                .animate({ fontSize: "2em" }, 1000);
+
         heading.css("color", "blue");
 
         setTimeout(function() {
